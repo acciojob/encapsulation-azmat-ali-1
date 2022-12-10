a877d2b4d187bd8b8b8ec0554ad9eb0a2f46dc41
AZMAT ALI <222azmat@gmail.com>
2022-12-10 19:15:07 +0530

    1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly A = new RWOnly();
+       A.setName("Azmat");
+        System.out.println(A.getName());
+    }
 }
